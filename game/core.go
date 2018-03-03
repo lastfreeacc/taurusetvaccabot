c19e6c31d0e7100236cf1a750038c6558cdf5c71
Alexander V Korpusev <akorpusev@tsc.ts>
2018-03-03 14:16:35 +0300

    for save

--- game/core.go
+++ game/core.go
@@ -4,31 +4,43 @@ import (
 	"errors"
 	"log"
 	"strconv"
+
+	"github.com/lastfreeacc/taurusetvaccabot/teleapi"
 )
 
+// Game ...
 type Game interface {
 	Play()
 }
 
 type game struct {
+	bot teleapi.Bot
 	ownerID  int64
 	callerID int64
 }
 
 func (g game) Play() {
-
+	ownderCh := make(chan string)
+	callerCh := make(chan string)
+	go g.toOwnerSender(ownderCh)
+	go g.toCallerSender(callerCh)
+	
 }
 
 var (
+	// ErrBadUserID means Bad user id
 	ErrBadUserID = errors.New("Bad user id")
+	// ErrBadNumber ... 
+	// TODO: never used
 	ErrBadNumber = errors.New("Bad number")
 )
 
-func New(ownerID, callerID int64) (Game, error) {
+// New creates Game
+func New(bot teleapi.Bot, ownerID, callerID int64) (Game, error) {
 	if ownerID == 0 || callerID == 0 {
 		return nil, ErrBadUserID
 	}
-	return game{ownerID: ownerID, callerID: callerID}, nil
+	return game{bot: bot, ownerID: ownerID, callerID: callerID}, nil
 }
 
 func countTandC(str string) (t, c int, err error) {
@@ -36,6 +48,7 @@ func countTandC(str string) (t, c int, err error) {
 	if err != nil {
 		return 0, 0, err
 	}
+	// TODO: need to implement
 	return 0, 0, nil
 }
 
@@ -63,3 +76,30 @@ func isValidNumber(str string) bool {
 // func (g game) diceFirstMove() bool {
 // 	Rando
 // }
+
+func sendToPleer(bot teleapi.Bot, pleerID int64, msg string) {
+	if pleerID == 0 {
+		return
+	}
+	if msg == "" {
+		return
+	}
+	req := teleapi.SendMessageReq{ChatID: pleerID, Text: msg}
+	bot.SendMessage(req)
+}
+
+// func readFromPleer()
+
+func (g game) toOwnerSender (c chan string) {
+	for {
+		msg := <-c
+		sendToPleer(g.bot, g.ownerID, msg)
+	}
+}
+
+func (g game) toCallerSender (c chan string) {
+	for {
+		msg := <-c
+		sendToPleer(g.bot, g.callerID, msg)
+	}
+}
